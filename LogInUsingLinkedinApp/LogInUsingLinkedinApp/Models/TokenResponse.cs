867f0c90bd5ae21809116ebb0d606b3576b0335a
Asitava Mahapatra <43506458+asitavamahapatra@users.noreply.github.com>
2019-02-20 22:22:15 -0500

    [ADD]: initial commit added the project

---
+++ LogInUsingLinkedinApp/LogInUsingLinkedinApp/Models/TokenResponse.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+
+namespace LogInUsingLinkedinApp.Models
+{
+    public class TokenResponse
+    {
+        [JsonProperty(PropertyName = "access_token")]
+        public string Access_token { get; set;}
+
+        [JsonProperty(PropertyName = "expires_in")]
+        public int Expires_in { get; set; }
+
+    }
+}
